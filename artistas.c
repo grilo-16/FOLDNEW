984d3b0ec1199b197edd88f95821e83b3cccfba2
grilo-16 <142771408+grilo-16@users.noreply.github.com>
2024-04-26 04:44:38 -0300

    artistas.c

---
+++ artistas.c
@@ -0,0 +1,40 @@
+#include "artistas.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void salvar_artistas(char *nome_arquivo, Artista artistas[], int num_artistas) {
+    // Implementação da função...
+}
+
+void ler_artistas(char *nome_arquivo, Artista artistas[], int *num_artistas) {
+    // Implementação da função...
+}
+
+int comparar_artistas(const void *a, const void *b) {
+    // Implementação da função...
+}
+
+void inserir_artista(Artista artistas[], int *num_artistas) {
+    // Implementação da função...
+}
+
+void removedor_artista(Artista artistas[], int *num_artistas) {
+    // Implementação da função...
+}
+
+void editar_artista(Artista artistas[], int num_artistas) {
+    // Implementação da função...
+}
+
+void buscar_binaria_artista(Artista artistas[], int num_artistas) {
+    // Implementação da função...
+}
+
+void buscar_sequencial_album(Artista artistas[], int num_artistas) {
+    // Implementação da função...
+}
+
+void mostrar_menu() {
+    // Implementação da função...
+}
